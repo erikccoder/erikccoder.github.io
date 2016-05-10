db00cea2db8b2b04ec8e63157fa1d6dd8a389cd9
erik <erikccoder@users.noreply.github.com>
2016-05-09 22:35:43 -0600

    Update GrabVideo.js

--- tools/GrabVideo.js
+++ tools/GrabVideo.js
@@ -1,46 +1,51 @@
 ((_window, _document) => {
-	var $ = function(selector){ return _document.querySelectorAll(selector); };
-	var doReset = _window.location.search.match("ek=clear");
-	if(!doReset && localStorage.ek)
-	{
-		var ek = JSON.parse(localStorage.ek),
-			list = $(ek.selector),
-			ele = list[ek.index];
-		if(ele)
-		{
-			if(ele.src){
-				_window.location = ele.src;
-				return;
-			}
-			else{
-				alert("Click Play button");
-			}
-		}
-	}
+    if (!NodeList.prototype.forEach) {
+        NodeList.prototype.forEach = function(a) {
+            for (var b = 0; b < this.length; b++) a(this[b], b, this)
+        };
+    }
+    var $ = function(selector) {
+        return _document.querySelectorAll(selector);
+    };
+    var doReset = _window.location.search.match("ek=clear");
+    if (!doReset && localStorage.ek) {
+        var ek = JSON.parse(localStorage.ek),
+            list = $(ek.selector),
+            ele = list[ek.index];
+        if (ele) {
+            if (ele.src) {
+                _window.location = ele.src;
+                return;
+            } else {
+                alert("Click Play button");
+            }
+        }
+    }
 
-	var searchNode = ["video", "source", "iframe"];
+    var searchNode = ["video", "source", "iframe"];
 
-	searchNode.forEach((selector) => {
-		$(selector).forEach((ele, index) => {	
-			
-			if (ele.src)
-			{
-				_document.write("<div><a data-selector='"+selector+"' data-index='"+index+"' href='"+ele.src+"'>"+ ele.src +"</a></div><hr>");
-			}
-		});
-	});
+    searchNode.forEach((selector) => {
+        $(selector)
+            .forEach((ele, index) => {
 
-	$("a").forEach((ele) => {
-		ele.addEventListener("click", 
-		(evt)=>{
-			evt.preventDefault();
-			var data = evt.currentTarget.dataset;
-			localStorage.ek = JSON.stringify({
-				"selector" : data.selector,
-				"index" : data.index
-			});
-			
-			return;
-		})
-	});
+                if (ele.src) {
+                    _document.write("<div><a data-selector='" + selector + "' data-index='" + index + "' href='" + ele.src + "'>" + ele.src + "</a></div><hr>");
+                }
+            });
+    });
+
+    $("a")
+        .forEach((ele) => {
+            ele.addEventListener("click",
+                (evt) => {
+                    evt.preventDefault();
+                    var data = evt.currentTarget.dataset;
+                    localStorage.ek = JSON.stringify({
+                        "selector": data.selector,
+                        "index": data.index
+                    });
+
+                    return;
+                })
+        });
 })(window, document);
